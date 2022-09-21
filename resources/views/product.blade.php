f6fc5071f0e0b948cd19504483bab03e11f0f570
kico0304 <kico_jajcanin@hotmail.com>
2022-09-21 22:51:48 +0200

    test 5 stojan

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -76,7 +76,7 @@
                     </div>
                 @endforeach
                 </div>
-                <div class="col-lg-2 mb-5 flexCenter">
+                <div class="col-lg-12 mb-5 flexCenter">
                     <img src="{{asset('images/products/product_'.$product->id.'/'.$product->house_foundation)}}">
                 </div>
                 <div class="divider mx-auto my-4"></div>
